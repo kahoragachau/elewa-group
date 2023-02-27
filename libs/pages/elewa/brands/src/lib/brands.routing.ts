6fa583d8326a547d9f50e143747f54334410d03b
kahoragachau <joelkahoragachau6@gmail.com>
2023-02-27 14:02:05 +0300

    ✨chore: Configure Routing file

--- libs/pages/elewa/brands/src/lib/brands.routing.ts
+++ libs/pages/elewa/brands/src/lib/brands.routing.ts
@@ -1,1 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
 
+import { BrandsPageComponent } from './pages/brands-page/brands-page.component';
+
+export const ELEWA_BRANDS_ROUTES: Route[] = [
+  { path: '', component:BrandsPageComponent }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(ELEWA_BRANDS_ROUTES)],
+  exports: [RouterModule]
+})
+
+export class BrandsRoutingModule { }
--- libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
+++ libs/pages/elewa/brands/src/lib/pages-elewa-brands.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@elewa-group/elements/layout';
+
 import { BrandsPageComponent } from './pages/brands-page/brands-page.component';
+import { BrandsRoutingModule } from './brands.routing';
 
 @NgModule({
-  imports: [CommonModule,LayoutModule],
+  imports: [CommonModule,LayoutModule,BrandsRoutingModule],
   declarations: [BrandsPageComponent],
+  exports: [BrandsPageComponent]
 })
 export class PagesElewaBrandsModule {}
